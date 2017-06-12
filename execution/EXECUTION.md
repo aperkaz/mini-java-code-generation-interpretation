95985ebb663a2e7c6c4b4392d4cb94e4ca20e88a
aperkaz <alainperkaz@gmail.com>
2017-06-12 11:47:10 +0200

    Add execution files and instructions

---
+++ execution/EXECUTION.md
@@ -0,0 +1,14 @@
+# Execution
+The folder contains two jars
+- `CodeGen.jar`, code generation for a file. Outputs  `.tjc` file
+- `Interpreter.jar`, code interpretation for a files
+
+## Running
+Code generator:
+```
+java -jar CodeGen.jar [JavaFile.jar]
+```
+Interpreter:
+```
+java -jar Interpreter.jar [TJC.tjc]
+```
---
+++ execution/Sum.java
@@ -0,0 +1,20 @@
+
+// A test program for TinyJava
+
+class Sum {
+  public static void main(String[] a){
+	 System.out.println(new Test().sum(10));
+  }
+}
+
+class Test {
+  int sum(int num) {
+    int sum;
+    sum = 0;
+    while (0 < num) {
+      sum = sum + num;
+      num = num - 1;
+	 }
+    return sum;
+  }
+}
